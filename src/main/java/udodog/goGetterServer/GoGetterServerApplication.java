487e81fffb0cc6e92b6943987d3e59c0d4b0c942
woo00oo <96woo94@naver.com>
2021-06-06 11:12:34 +0900

    스프링부트 환경 설정(Lombok, Spring Web, Spring Data JPA, DevTools 의존성 추가)

---
+++ src/main/java/udodog/goGetterServer/GoGetterServerApplication.java
@@ -0,0 +1,13 @@
+package udodog.goGetterServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoGetterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoGetterServerApplication.class, args);
+	}
+
+}
