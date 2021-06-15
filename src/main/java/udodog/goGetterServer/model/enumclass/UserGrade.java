93db0bf80224f4fb89d1b06b97fe65a9c2d36cd6
woo00oo <96woo94@naver.com>
2021-06-15 14:46:01 +0900

    사용자 등급 UserCrade Enum 클래스 구현

--- src/main/java/udodog/goGetterServer/model/enumclass/UserGrade.java
+++ src/main/java/udodog/goGetterServer/model/enumclass/UserGrade.java
@@ -1,2 +1,17 @@
-package udodog.goGetterServer.model.enumclass;public enum UserGrade {
+package udodog.goGetterServer.model.enumclass;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public enum UserGrade {
+
+    USER("일반회원"),
+    ADMIN("관리자")
+    ;
+
+    private String description;
 }
