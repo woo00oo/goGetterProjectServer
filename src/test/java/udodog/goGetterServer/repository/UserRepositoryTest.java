43b060dcc0b9fda42b160b86505ae26b523a4463
woo00oo <96woo94@naver.com>
2021-06-15 14:46:32 +0900

    User 엔티티 DataJpaTest 테스트 코드 작성

--- src/test/java/udodog/goGetterServer/repository/UserRepositoryTest.java
+++ src/test/java/udodog/goGetterServer/repository/UserRepositoryTest.java
@@ -1,2 +1,43 @@
-package udodog.goGetterServer.repository;public class UserRepositroyTest {
+package udodog.goGetterServer.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import udodog.goGetterServer.model.entity.User;
+import udodog.goGetterServer.model.enumclass.UserGrade;
+
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+class UserRepositoryTest{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 회원_저장(){
+
+        //given
+        User user = User.builder()
+                .email("hwoo00oo96@gmail.com")
+                .password("1234")
+                .name("변현우")
+                .nickName("woo00oo")
+                .phoneNumber("010-9245-7396")
+                .grade(UserGrade.USER)
+                .build();
+
+        //when
+        User saveUser = userRepository.save(user);
+
+        //then
+        assertThat(user).isEqualTo(saveUser);
+
+    }
 }
