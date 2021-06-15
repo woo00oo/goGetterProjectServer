f8b75da338a20713171f34160a9097d52482e27b
woo00oo <96woo94@naver.com>
2021-06-16 00:28:56 +0900

    CouponBox 엔티티, 레파지토리 구현 및 테스트코드 작성

---
+++ src/test/java/udodog/goGetterServer/repository/CouponBoxRepositoryTest.java
@@ -0,0 +1,66 @@
+package udodog.goGetterServer.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import udodog.goGetterServer.model.entity.Coupon;
+import udodog.goGetterServer.model.entity.CouponBox;
+import udodog.goGetterServer.model.entity.User;
+import udodog.goGetterServer.model.enumclass.UserGrade;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CouponBoxRepositoryTest {
+
+    @Autowired
+    private CouponBoxRepository couponBoxRepository;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void 쿠폰함_저장(){
+
+        //given
+        Coupon coupon = Coupon.builder()
+                .name("문화상품권")
+                .serialNumber(1234512345L)
+                .validDate(30)
+                .build();
+
+        User user = User.builder()
+                .email("hwoo00oo96@gmail.com")
+                .password("1234")
+                .name("변현우")
+                .nickName("woo00oo")
+                .phoneNumber("010-9245-7396")
+                .grade(UserGrade.USER)
+                .build();
+
+
+        User saveUser = userRepository.save(user);
+        Coupon saveCoupon = couponRepository.save(coupon);
+
+        CouponBox couponBox = CouponBox.builder()
+                .user(saveUser)
+                .coupon(saveCoupon)
+                .endDate(LocalDate.of(2021,07,15))
+                .build();
+
+        //when
+        CouponBox saveCouponBox = couponBoxRepository.save(couponBox);
+
+        //then
+
+        assertThat(couponBox).isEqualTo(saveCouponBox);
+    }
+}
