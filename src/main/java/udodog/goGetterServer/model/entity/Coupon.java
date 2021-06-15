a4ed5a837d8647fd3483e8a534747ebf1bad9995
woo00oo <96woo94@naver.com>
2021-06-15 23:53:05 +0900

    Coupon 엔티티, 레파지토리 구현 및 테스트코드 작성

---
+++ src/main/java/udodog/goGetterServer/model/entity/Coupon.java
@@ -0,0 +1,34 @@
+package udodog.goGetterServer.model.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Long serialNumber;
+
+    private Integer validDate;
+
+    @Builder
+    public Coupon(Long id, String name, Long serialNumber, Integer validDate) {
+        this.id = id;
+        this.name = name;
+        this.serialNumber = serialNumber;
+        this.validDate = validDate;
+    }
+}
