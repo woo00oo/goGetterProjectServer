6d774a8fc705c87de64c5d054578fa2c42036a03
woo00oo <96woo94@naver.com>
2021-06-16 00:28:56 +0900

    CouponBox 엔티티, 레파지토리 구현 및 테스트코드 작성

---
+++ src/main/java/udodog/goGetterServer/model/entity/CouponBox.java
@@ -0,0 +1,36 @@
+package udodog.goGetterServer.model.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class CouponBox {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(targetEntity = Coupon.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "coupon_id")
+    private Coupon coupon;
+
+    private LocalDate endDate;
+
+    @Builder
+    public CouponBox(Long id, User user, Coupon coupon, LocalDate endDate) {
+        this.id = id;
+        this.user = user;
+        this.coupon = coupon;
+        this.endDate = endDate;
+    }
+}
