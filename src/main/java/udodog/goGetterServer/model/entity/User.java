f3ce58efb6a2f15a08758270bcc6c9ddb822ebda
woo00oo <96woo94@naver.com>
2021-06-15 14:45:38 +0900

    User 엔티티 구현

--- src/main/java/udodog/goGetterServer/model/entity/User.java
+++ src/main/java/udodog/goGetterServer/model/entity/User.java
@@ -1,2 +1,55 @@
-package udodog.goGetterServer.model.entity;public class User {
+package udodog.goGetterServer.model.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import udodog.goGetterServer.model.enumclass.UserGrade;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String nickName;
+
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    private UserGrade grade;
+
+    private String token;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    private String profileUrl;
+
+    @Builder
+    public User(Long id, String email, String password, String name, String nickName, String phoneNumber,
+                UserGrade grade, LocalDate createdAt) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+        this.grade = grade;
+        this.createdAt = createdAt;
+    }
 }
