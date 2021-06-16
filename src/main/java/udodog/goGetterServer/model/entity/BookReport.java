8b90ff7cc6e0daddaea01e7d0347cb8efb1b5a99
woo00oo <96woo94@naver.com>
2021-06-16 11:31:21 +0900

    Book Report 엔티티, 레파지토리 구현 및 테스트 코드 작성

---
+++ src/main/java/udodog/goGetterServer/model/entity/BookReport.java
@@ -0,0 +1,46 @@
+package udodog.goGetterServer.model.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class BookReport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(targetEntity = Book.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne(targetEntity = Book.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Builder
+    public BookReport(Long id, Book book, User user, String title, String content, LocalDateTime createdAt) {
+        this.id = id;
+        this.book = book;
+        this.user = user;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+}
