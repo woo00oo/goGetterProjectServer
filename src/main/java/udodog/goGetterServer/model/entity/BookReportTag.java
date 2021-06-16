33eaddd51068de0f595c83111cfcf9156cd20ab3
woo00oo <96woo94@naver.com>
2021-06-16 11:49:14 +0900

    BookReportTag 엔티티, 레파지토리 구현 및 테스트 코드 작성

---
+++ src/main/java/udodog/goGetterServer/model/entity/BookReportTag.java
@@ -0,0 +1,31 @@
+package udodog.goGetterServer.model.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class BookReportTag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(targetEntity = BookReport.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_report_id")
+    private BookReport bookReport;
+
+    private String tagName;
+
+    @Builder
+    public BookReportTag(Long id, BookReport bookReport, String tagName) {
+        this.id = id;
+        this.bookReport = bookReport;
+        this.tagName = tagName;
+    }
+
+}
