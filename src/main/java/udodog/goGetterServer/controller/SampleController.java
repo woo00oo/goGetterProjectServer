6e27b9c4a5ae4e7c0b1d646206a5ca4b39633022
kimyubi <ieieie0419@gmail.com>
2021-06-23 20:12:25 +0900

    Chore #28 : ModelMapper 의존성 추가 및 테스트

--- src/main/java/udodog/goGetterServer/controller/SampleController.java
+++ src/main/java/udodog/goGetterServer/controller/SampleController.java
@@ -1,17 +1,18 @@
 package udodog.goGetterServer.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import udodog.goGetterServer.model.converter.EntityToModelConvertor;
 import udodog.goGetterServer.model.dto.DefaultRes;
+import udodog.goGetterServer.model.dto.ModelMapperTestDTO;
 import udodog.goGetterServer.model.entity.Book;
-
+import udodog.goGetterServer.model.entity.SharingBoard;
 
 
 @Slf4j
@@ -20,6 +21,10 @@ public class SampleController {
 
     @Autowired
     private EntityToModelConvertor entityToModelConvertor;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
 
     @GetMapping("/sample")
     public ResponseEntity<EntityModel<DefaultRes<Book>>> getBook(){
@@ -30,6 +35,16 @@ public class SampleController {
                         DefaultRes.response(HttpStatus.OK.value(), "정상 조회되었습니다",book)
                 ));
 
+    }
+
+    @GetMapping("/test-mapper")
+    public ResponseEntity<DefaultRes<ModelMapperTestDTO>> getModelMapperTestDTO(){
+        SharingBoard sharingBoard = new SharingBoard();
+        ModelMapperTestDTO dto = modelMapper.map(sharingBoard, ModelMapperTestDTO.class);
+
+        return ResponseEntity
+                .ok(DefaultRes.response(HttpStatus.OK.value(), "정상 조회되었습니다",dto)
+                );
     }
 
 }
