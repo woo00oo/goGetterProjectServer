4e62f7cadd4baafc704a48fdfe14001c9b1585da
JunyHarang <76773429+JunyHarang@users.noreply.github.com>
2021-06-25 00:37:47 +0900

    Update BlackMemberManagementService.java

--- src/main/java/udodog/goGetterServer/service/blackmember/BlackMemberManagementService.java
+++ src/main/java/udodog/goGetterServer/service/blackmember/BlackMemberManagementService.java
@@ -1,18 +1,14 @@
 package udodog.goGetterServer.service.blackmember;
 
 import lombok.RequiredArgsConstructor;
-<<<<<<< Updated upstream
+
 import org.modelmapper.ModelMapper;
-=======
->>>>>>> Stashed changes
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-<<<<<<< Updated upstream
 import udodog.goGetterServer.config.ModelMapperConfig;
-=======
->>>>>>> Stashed changes
 import udodog.goGetterServer.model.dto.DefaultRes;
 import udodog.goGetterServer.model.dto.Pagination;
 import udodog.goGetterServer.model.dto.response.BlackMemberManagementResDto;
@@ -28,31 +24,20 @@ public class BlackMemberManagementService {
 
     private final BlackMemberManagementRepository blackMemberManagementRepository;      // 블랙 회원 Repository 사용을 위한 인스턴스 변수 선언
 
-<<<<<<< Updated upstream
     private final ModelMapper modelMapper;
     public List<BlackMemberManagementResDto> res(Page<BlackMemberManagement> page) {
 
         List<BlackMemberManagementResDto> blackMemberList = page.stream().map(p -> modelMapper.map(p, BlackMemberManagementResDto.class)).collect(Collectors.toList());
 
         return blackMemberList;
-=======
 
-    public List<BlackMemberManagementResDto> res(Page<BlackMemberManagement> page) {
-
-        List<BlackMemberManagementResDto> noticeList = page.stream().map(p -> )     collect(Collectors.toList());
-
-        return noticeList;
->>>>>>> Stashed changes
     }
 
 
     public DefaultRes<List<BlackMemberManagementResDto>> findAllBlackMember(Pageable pageable) {
 
-<<<<<<< Updated upstream
-        Page<BlackMemberManagement> blackMemberPage = blackMemberManagementRepository.findAll(pageable);
-=======
         Page<BlackMemberManagement> blackMemberPage = blackMemberManagementRepository.findByAll(pageable);
->>>>>>> Stashed changes
+
 
         if(blackMemberPage.getTotalElements() == 0) {
             return DefaultRes.response(HttpStatus.OK.value(), "정보없음");
