802d6d4cb18f102374cd0613d4c94d8e0f3bd8b8
junhwan <torvlf@naver.com>
2021-06-24 17:32:50 +0900

    feat:#33: Discussion rest api - 상세보기 api 기능 구현

---
+++ src/main/java/udodog/goGetterServer/model/converter/discussion/DiscussionConvertor.java
@@ -0,0 +1,22 @@
+package udodog.goGetterServer.model.converter.discussion;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+import udodog.goGetterServer.controller.api.discussion.DiscussionController;
+import udodog.goGetterServer.model.dto.DefaultRes;
+import udodog.goGetterServer.model.dto.response.discussion.DiscussionDetailResponse;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class DiscussionConvertor implements RepresentationModelAssembler<DefaultRes<DiscussionDetailResponse>, EntityModel<DefaultRes<DiscussionDetailResponse>>> {
+
+    @Override
+    public EntityModel<DefaultRes<DiscussionDetailResponse>> toModel(DefaultRes<DiscussionDetailResponse> entity) {
+
+        return EntityModel.of(entity,
+                linkTo(methodOn(DiscussionController.class).getDetailBoard(null)).withRel("detail"));
+    }
+}
---
+++ src/main/java/udodog/goGetterServer/model/dto/response/discussion/DiscussionDetailResponse.java
@@ -0,0 +1,29 @@
+package udodog.goGetterServer.model.dto.response.discussion;
+
+import lombok.*;
+import udodog.goGetterServer.model.entity.DiscussionBoard;
+import udodog.goGetterServer.model.entity.User;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@ToString
+public class DiscussionDetailResponse {
+
+    private Long id;
+    private User user;
+    private String title;
+    private String content;
+    private LocalDate createAt;
+
+
+    @Builder
+    public DiscussionDetailResponse(DiscussionBoard discussionBoard){
+        this.id = discussionBoard.getId();
+        this.user = discussionBoard.getUser();
+        this.title = discussionBoard.getTitle();
+        this.content = discussionBoard.getContent();
+        this.createAt = discussionBoard.getCreateAt();
+    }
+}
