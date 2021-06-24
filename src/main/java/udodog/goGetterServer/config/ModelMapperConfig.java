91e5b7da49cdd203a52751530a812d921e0b31e8
junhwan <torvlf@naver.com>
2021-06-24 16:57:58 +0900

    feat:#27: Discussion rest api - 전체 목록 조회 api 기능 구현

--- src/main/java/udodog/goGetterServer/config/ModelMapperConfig.java
+++ src/main/java/udodog/goGetterServer/config/ModelMapperConfig.java
@@ -1,17 +1,9 @@
 package udodog.goGetterServer.config;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> adf01ba223133459adf4a37c3c4be9ef9234264a
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> adf01ba223133459adf4a37c3c4be9ef9234264a
 @Configuration
 public class ModelMapperConfig {
 
