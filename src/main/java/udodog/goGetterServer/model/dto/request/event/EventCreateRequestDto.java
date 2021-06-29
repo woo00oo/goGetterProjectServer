c31cf7f9372078495e39ece279a4a57cfb6dd3b9
woo00oo <96woo94@naver.com>
2021-06-29 22:27:50 +0900

    Feat #79: 이벤트 등록 요청 DTO 구현

---
+++ src/main/java/udodog/goGetterServer/model/dto/request/event/EventCreateRequestDto.java
@@ -0,0 +1,44 @@
+package udodog.goGetterServer.model.dto.request.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import udodog.goGetterServer.model.entity.Event;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventCreateRequestDto {
+
+    @NotEmpty
+    private String title;
+
+    @NotEmpty
+    private String content;
+
+    @FutureOrPresent
+    private LocalDate startDate;
+
+    @FutureOrPresent
+    private LocalDate endDate;
+
+    private String imgUrl;
+
+    @Builder
+    public Event toEntity(){
+
+        return Event.builder()
+                .title(title)
+                .content(content)
+                .startDate(startDate)
+                .endDate(endDate)
+                .imgUrl(imgUrl)
+                .build();
+
+    }
+}
