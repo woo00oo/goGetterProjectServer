1561c607c884f21c6816d067e6aac0a9bd76b2d0
woo00oo <96woo94@naver.com>
2021-07-01 02:15:35 +0900

    Chore #84: Querydsl 단순 조회 테스트 코드 작성

--- src/test/java/udodog/goGetterServer/repository/UserRepositoryTest.java
+++ src/test/java/udodog/goGetterServer/repository/UserRepositoryTest.java
@@ -1,5 +1,7 @@
 package udodog.goGetterServer.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -7,6 +9,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import udodog.goGetterServer.config.JpaAuditingConfig;
+import udodog.goGetterServer.config.QuerydslConfig;
+import udodog.goGetterServer.model.entity.QEvent;
+import udodog.goGetterServer.model.entity.QUser;
 import udodog.goGetterServer.model.entity.User;
 import udodog.goGetterServer.model.enumclass.UserGrade;
 
@@ -15,13 +20,16 @@ import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTest
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(
         type = FilterType.ASSIGNABLE_TYPE,
-        classes = JpaAuditingConfig.class
+        classes = {JpaAuditingConfig.class, QuerydslConfig.class}
 ))
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 class UserRepositoryTest{
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private JPAQueryFactory jpaQueryFactory;
 
     @Test
     void 회원_저장(){
@@ -41,6 +49,34 @@ class UserRepositoryTest{
 
         //then
         assertThat(user).isEqualTo(saveUser);
+
+    }
+
+    @Test
+    void 회원_조회(){
+        //given
+        User user = User.builder()
+                .email("hwoo00oo96@gmail.com")
+                .password("1234")
+                .name("변현우")
+                .nickName("woo00oo")
+                .phoneNumber("010-9245-7396")
+                .grade(UserGrade.USER)
+                .build();
+
+        User saveUser = userRepository.save(user);
+
+        //when
+
+        QUser qUser = QUser.user;
+
+        QEvent event = QEvent.event;
+        User findUser = jpaQueryFactory.selectFrom(qUser)
+                .where(qUser.email.eq("hwoo00oo96@gmail.com"))
+                .fetchOne();
+
+        //then
+        Assertions.assertThat(findUser).isEqualTo(user);
 
     }
 }
