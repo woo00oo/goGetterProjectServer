a436221e8fa85bfe6714033509362d76cb59aa18
woo00oo <96woo94@naver.com>
2021-07-02 00:35:09 +0900

    Feat #91: 종료된 이벤트 전체조회 API 레파지토리 쿼리 작성

--- src/main/java/udodog/goGetterServer/repository/querydsl/EventQueryRepository.java
+++ src/main/java/udodog/goGetterServer/repository/querydsl/EventQueryRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-import udodog.goGetterServer.model.dto.response.event.ProgressEventsResponseDto;
+import udodog.goGetterServer.model.dto.response.event.EventsResponseDto;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,16 +20,35 @@ public class EventQueryRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public Page<ProgressEventsResponseDto> progressEventFindAll(Pageable pageable){
+    public Page<EventsResponseDto> progressEventFindAll(Pageable pageable){
 
-        List<ProgressEventsResponseDto> eventList = queryFactory
-                .select(Projections.constructor(ProgressEventsResponseDto.class,
+        List<EventsResponseDto> eventList = queryFactory
+                .select(Projections.constructor(EventsResponseDto.class,
                         event.id.as("eventId"),
                         event.title,
                         event.startDate,
                         event.endDate))
                 .from(event)
                 .where(event.endDate.after(LocalDate.now().minusDays(1)))
+                .fetch();
+
+        int start = (int)pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), eventList.size());
+
+        return new PageImpl<>(eventList.subList(start, end), pageable, eventList.size());
+
+    }
+
+    public Page<EventsResponseDto> endEventFindAll(Pageable pageable){
+
+        List<EventsResponseDto> eventList = queryFactory
+                .select(Projections.constructor(EventsResponseDto.class,
+                        event.id.as("eventId"),
+                        event.title,
+                        event.startDate,
+                        event.endDate))
+                .from(event)
+                .where(event.endDate.before(LocalDate.now().minusDays(1)))
                 .fetch();
 
         int start = (int)pageable.getOffset();
