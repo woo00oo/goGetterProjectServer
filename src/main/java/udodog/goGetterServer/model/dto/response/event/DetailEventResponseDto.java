55c972b1e092a4943b75fd9b7dcba64cbed37780
woo00oo <96woo94@naver.com>
2021-07-02 11:18:40 +0900

    Feat #93: 진행중인 이벤트 상세 조회 API 응답 DTO 구현

---
+++ src/main/java/udodog/goGetterServer/model/dto/response/event/DetailEventResponseDto.java
@@ -0,0 +1,34 @@
+package udodog.goGetterServer.model.dto.response.event;
+
+import lombok.Getter;
+import udodog.goGetterServer.model.entity.Event;
+
+import java.time.LocalDate;
+
+@Getter
+public class DetailEventResponseDto {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private String imgUrl;
+
+    private Long couponBoxId;
+
+    public DetailEventResponseDto(Event event) {
+        this.id = event.getId();
+        this.title = event.getTitle();
+        this.content = event.getContent();
+        this.startDate = event.getStartDate();
+        this.endDate = event.getEndDate();
+        this.imgUrl = event.getImgUrl();
+        this.couponBoxId = event.getCouponBoxId();
+    }
+}
