08736dcd68f4726cd3d36d6b20bcdf9eb5cd4006
junhwan <torvlf@naver.com>
2021-07-02 17:25:02 +0900

    refactor:#103 : DiscussionBoard Refactor
    - DiscussionBoardQueryRepository 추가 및 적용
    - DiscussionBoardReplyQueryRepository 추가 및 적용
    - DiscussionBoardReadhitQueryRepository 추가 및 적용
    - Pagination 성능 개선

--- src/main/java/udodog/goGetterServer/controller/api/discussion/DiscussionReplyController.java
+++ src/main/java/udodog/goGetterServer/controller/api/discussion/DiscussionReplyController.java
@@ -36,7 +36,7 @@ public class DiscussionReplyController {
     })
 
     // 글 등록 Controller
-    @PostMapping("/api/user/discussionreplies")
+    @PostMapping("/api/users/discussionreplies")
     public ResponseEntity<EntityModel<DefaultRes<DiscussionReplyResponse>>> createReply(
             @ApiParam("필수 : 모든사항")
             @RequestBody DiscussionReplyInsertRequest requestDto,
@@ -52,10 +52,10 @@ public class DiscussionReplyController {
     })
 
     // 댓글 조회 Controller
-    @GetMapping("/api/bkuser/discussionreplies")
+    @GetMapping("/api/bkusers/discussionreplies")
     public ResponseEntity<EntityModel<DefaultRes<List<DiscussionReplyResponse>>>> getBoardReplyList(
             @RequestParam("discussionId") Long discussionId,
-            @PageableDefault(sort = "createAt", direction = Sort.Direction.DESC, size = 3) Pageable pageable // 최신 날짜순으로 내림차순, 페이지당 3개씩 출력
+            @PageableDefault(sort = "id", direction = Sort.Direction.DESC, size = 3) Pageable pageable // 최신 날짜순으로 내림차순, 페이지당 3개씩 출력
     ){
         return new ResponseEntity<>(replyListConvertor.toModel(replyService.getBoardReplyList(discussionId, pageable)), HttpStatus.OK);
     }
@@ -66,7 +66,7 @@ public class DiscussionReplyController {
     })
 
     // 댓글 수정 Controller
-    @PutMapping("/api/user/discussionreplies")
+    @PutMapping("/api/users/discussionreplies")
     public ResponseEntity<EntityModel<DefaultRes<DiscussionReplyEditRequest>>> updateReply(
             @RequestParam("replyId") Long replyId,
             @RequestParam("userId") Long userId,
@@ -77,11 +77,11 @@ public class DiscussionReplyController {
 
     @ApiOperation(value = "토론게시판 댓글삭제 API",notes = "댓글삭제 API입니다.")
     @ApiResponses(value ={
-            @ApiResponse(code=200, message = "1.삭제성공 \\t\\n 2. 삭제실패 \\t\\n 3. 데이터없음 \\t\\n 4. 토큰에러")
+            @ApiResponse(code=200, message = "1.삭제성공 \t\n 2. 삭제실패 \t\n 3. 데이터없음 \t\n 4. 토큰에러")
     })
 
     // 댓글 삭제 Controller
-    @DeleteMapping("/api/user/discussionreplies")
+    @DeleteMapping("/api/users/discussionreplies")
     public ResponseEntity<EntityModel<DefaultRes<DiscussionBoardReply>>> deleteReply (@RequestParam("replyId") Long replyId,
                                                                                       @RequestParam("userId") Long userId){
         return new ResponseEntity<>(replyConvertor.toModel(replyService.delete(replyId, userId)), HttpStatus.OK);
