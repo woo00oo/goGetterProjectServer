d4c1137757bb176ff602667a3722c9c4172c5b07
woo00oo <96woo94@naver.com>
2021-07-06 14:17:59 +0900

    Test #noisuue:  독서 공유 관련 기능 테스트 코드 수정

--- src/main/java/udodog/goGetterServer/model/dto/request/sharingboard/CreateBoardRequest.java
+++ src/main/java/udodog/goGetterServer/model/dto/request/sharingboard/CreateBoardRequest.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotEmpty;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CreateBoardRequest {
 
     @NotEmpty
--- src/test/java/udodog/goGetterServer/controller/api/sharingboard/SharingBoardControllerTest.java
+++ src/test/java/udodog/goGetterServer/controller/api/sharingboard/SharingBoardControllerTest.java
@@ -3,23 +3,16 @@ package udodog.goGetterServer.controller.api.sharingboard;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import udodog.goGetterServer.model.entity.SharingBoard;
-import udodog.goGetterServer.model.entity.SharingBoardReply;
-import udodog.goGetterServer.model.entity.User;
-import udodog.goGetterServer.model.enumclass.UserGrade;
-import udodog.goGetterServer.repository.UserRepository;
+import udodog.goGetterServer.config.WebMvcConfig;
+import udodog.goGetterServer.model.converter.sharingboard.SharingConverter;
+import udodog.goGetterServer.model.converter.sharingboard.SharingListConverter;
 import udodog.goGetterServer.service.sharingboard.SharingBoardService;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,10 +25,19 @@ public class SharingBoardControllerTest {
     @Autowired private MockMvc mockMvc;
     @MockBean private SharingBoardService sharingBoardService;
 
+    @MockBean
+    private SharingConverter sharingConverter;
+
+    @MockBean
+    private SharingListConverter sharingListConverter;
+
+    @MockBean
+    private WebMvcConfig webMvcConfig;
+
     @Test
     @DisplayName("공유 게시판 전체 조회 테스트")
     public void 전체조회_테스트() throws Exception {
-        mockMvc.perform(get("/sharings")
+        mockMvc.perform(get("/api/sharings")
                 .contentType(MediaTypes.ALPS_JSON_VALUE))
                 .andDo(print())
                 .andExpect(status().isOk());
