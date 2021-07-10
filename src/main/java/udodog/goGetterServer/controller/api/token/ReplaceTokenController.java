2cc0c13643f73dbe91aa979e612f2ae2117ffe29
woo00oo <96woo94@naver.com>
2021-07-10 18:48:57 +0900

    Docs #159: 엑세스 토큰 재발급 API 문서 수정

--- src/main/java/udodog/goGetterServer/controller/api/token/ReplaceTokenController.java
+++ src/main/java/udodog/goGetterServer/controller/api/token/ReplaceTokenController.java
@@ -3,6 +3,8 @@ package udodog.goGetterServer.controller.api.token;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,10 @@ public class ReplaceTokenController {
     private final SessionService sessionService;
 
     @ApiOperation(value = "엑세스 토큰 재발급 API",notes = "리프레시 토큰이 유효하다면 엑세스 토큰을 재발급 합니다.(유효하지 않다면 로그아웃 처리")
+    @ApiResponses(value ={
+            @ApiResponse(code=200, message = "1. 토큰재발급"),
+            @ApiResponse(code=401, message = "1. 요청에러 \n 2. 토큰불일치")
+    })
     @GetMapping("/token")
     public ResponseEntity<DefaultRes> replaceToken(
             HttpServletRequest request
