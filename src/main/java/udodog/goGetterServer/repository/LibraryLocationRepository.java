3a747133aba39a20d1256e895cfd7090551b0a0d
junyharang <junyharang8592@gmail.com>
2021-07-22 17:38:07 +0900

    Refactor #182 : 전국 도서관 정보 기능 Domin Layer 및 Repository Test 재 구성

--- src/main/java/udodog/goGetterServer/repository/LibraryLocationRepository.java
+++
@@ -1,7 +0,0 @@
-package udodog.goGetterServer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import udodog.goGetterServer.model.entity.LibraryLocation;
-
-public interface LibraryLocationRepository extends JpaRepository <LibraryLocation, Long> {
-} // Interface End
