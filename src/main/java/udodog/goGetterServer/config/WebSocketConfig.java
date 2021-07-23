80278acedb24fb40677bcd1b9eccb00573ed1f50
woo00oo <96woo94@naver.com>
2021-07-23 16:58:41 +0900

    Test #191 : 웹소켓 config 파일 수정

--- src/main/java/udodog/goGetterServer/config/WebSocketConfig.java
+++ src/main/java/udodog/goGetterServer/config/WebSocketConfig.java
@@ -12,13 +12,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic","/queue");
     }
 }
--- src/main/java/udodog/goGetterServer/service/message/MessageService.java
+++ src/main/java/udodog/goGetterServer/service/message/MessageService.java
@@ -94,7 +94,7 @@ public class MessageService {
     }
 
     public void save(Message message){
-
+        messageRepository.save(message);
     }
 
 }
