ca00a62476af1c05821f4203a922bdd76a82f98e
woo00oo <96woo94@naver.com>
2021-07-23 13:43:48 +0900

    Chore #189: 웹소켓 의존성 및 config 파일 추가

--- src/main/java/udodog/goGetterServer/config/SpringAsyncConfig.java
+++ src/main/java/udodog/goGetterServer/config/SpringAsyncConfig.java
@@ -2,6 +2,7 @@ package udodog.goGetterServer.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -13,6 +14,7 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class SpringAsyncConfig {
 
     @Bean
+    @Primary
     public Executor threadPoolTaskExecutor()
     {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
